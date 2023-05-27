b099fbbb24642ec51fa9ac98e0d4bc319d037a26
donghyun3996 <ehdgus3996@naver.com>
2023-05-27 17:10:01 +0900

    dk

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import Button from "./button";
 import styles from "./App.modul.css";
 import { useEffect, useState } from "react";
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [counter,setCounter] = useState(0);
-  const [keyWord, setKeyWord] = useState("");
-
-  const onClick = () => {
-      setCounter((v) => v+1);
-  };
-  const onChange = (event) =>{
-    setKeyWord(event.target.value);
-  }
-  useEffect(()=>{
-   console.log("once");
-  },[]);
-
-  useEffect(()=>{
-      console.log("serch" );
-    },[keyWord]);
-    useEffect(()=>{
-      console.log("count" );
-    },[counter]);
 
   return (
-    <div>
-    <input value={keyWord} onChange={onChange} type="text" placeholder="Surch here..."></input>
-    <h1>{counter}</h1>
-    <button onClick={onClick}>click</button>
-
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie" element={<Detail/>}/>
+      </Routes>
+    </Router>
+    );
 }
 
 export default App;
